5725a750fc15281974a6d20aadc137a8b8362e8e
ClockWize171 <jupiterminhtin998@gmail.com>
2022-04-11 13:39:59 +0630

    added header component and linked to home, shop pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { HomePage, ShopPage } from './containers'
 import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />}></Route>
         <Route path='/shop' element={<ShopPage />}></Route>
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as MenuItems} from './MenuItems/MenuItems';
 export {default as Directory} from './Directory/Directory';
 export {default as PreviewCollection} from './PreviewCollection/PreviewCollection';
+export {default as CollectionItem} from './CollectionItem/CollectionItem';
